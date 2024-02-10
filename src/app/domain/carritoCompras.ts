cfbfcea86907f5b0cc8b9a60f7e9b3040845d4ac
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-02-10 18:52:48 -0500

    2222
    carr

--- src/app/domain/carritoCompras.ts
+++ src/app/domain/carritoCompras.ts
@@ -1,10 +1,9 @@
 
 
 export class carritoCompras{
+    public car_idcarrito?: number;
+    public car_estado?: string;
+  
 
-    idCarrito_Compras: number=0;
-    codigo:string='';
-    fecha_Creacion:Date | undefined;
-    estado:string='';
 
 }
